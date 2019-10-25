c4afbb74bb139893dcdfd9954d3e500fa5b65ac1
jxh <xinyuemoli@foxmail.com>
2019-10-25 17:35:34 +0800

    提交代码

---
+++ fit&eat/kobe/src/main/java/com/f/a/kobe/config/IdWorkConfig.java
@@ -0,0 +1,16 @@
+package com.f.a.kobe.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.f.a.kobe.util.IdWorker;
+
+@Configuration
+public class IdWorkConfig {
+
+	@Bean
+	public IdWorker idWorker() {
+		return new IdWorker();
+	}
+
+}
