0b1e8f4f28131ce26fe2f017cdee10d53e752662
micezhao <micezhao@zhaochendeMacbook-pro.local>
2019-10-30 11:39:38 +0800

    修改baseManager

--- fit&eat/kobe/src/main/java/com/f/a/kobe/manager/CustomerCredentialManager.java
+++ fit&eat/kobe/src/main/java/com/f/a/kobe/manager/CustomerCredentialManager.java
@@ -2,47 +2,47 @@ package com.f.a.kobe.manager;
 
 import java.util.List;
 
-import com.f.a.kobe.basemanager.CustomerBaseManager;
 import com.f.a.kobe.pojo.CustomerCredential;
 
-public class CustomerCredentialManager implements CustomerBaseManager<CustomerCredential>{
+public class CustomerCredentialManager implements BaseManager<CustomerCredential>{
 
 	@Override
-	public CustomerCredential queryObjectById(Long id) {
+	public CustomerCredential queryById(Long id) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public CustomerCredential queryObjectByUserId(String userId) {
+	public CustomerCredential queryByBiz(Object bizId) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public List<CustomerCredential> queryListByConditional(CustomerCredential t) {
+	public List<CustomerCredential> listByConditional(CustomerCredential t) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public int insertObject(CustomerCredential t) {
+	public int insert(CustomerCredential t) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
-	public int updateObject(CustomerCredential t) {
+	public int update(CustomerCredential t) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
-	public int deleteObject(CustomerCredential t) {
+	public int delete(Long id) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
+	
 	
 
 }
