dbed318b2899e4d6c359ca4ce31573a0d25f374e
jxh <xinyuemoli@foxmail.com>
2019-10-30 11:30:33 +0800

    提交代码

---
+++ fit&eat/kobe/src/main/java/com/f/a/kobe/manager/CustomerCredentialManager.java
@@ -0,0 +1,48 @@
+package com.f.a.kobe.manager;
+
+import java.util.List;
+
+import com.f.a.kobe.basemanager.CustomerBaseManager;
+import com.f.a.kobe.pojo.CustomerCredential;
+
+public class CustomerCredentialManager implements CustomerBaseManager<CustomerCredential>{
+
+	@Override
+	public CustomerCredential queryObjectById(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public CustomerCredential queryObjectByUserId(String userId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<CustomerCredential> queryListByConditional(CustomerCredential t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int insertObject(CustomerCredential t) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int updateObject(CustomerCredential t) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int deleteObject(CustomerCredential t) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	
+
+}
