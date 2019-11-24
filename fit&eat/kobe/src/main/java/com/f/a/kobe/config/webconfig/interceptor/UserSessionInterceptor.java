60d0d7d41d9f0c4f79cd0ddfb89127afe04fd400
micezhao <zc_wuhan@163.com>
2019-11-24 09:40:36 +0800

    Update UserSessionInterceptor.java

--- fit&eat/kobe/src/main/java/com/f/a/kobe/config/webconfig/interceptor/UserSessionInterceptor.java
+++ fit&eat/kobe/src/main/java/com/f/a/kobe/config/webconfig/interceptor/UserSessionInterceptor.java
@@ -8,22 +8,20 @@ import org.apache.http.HttpStatus;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import com.alibaba.fastjson.JSONObject;
+import com.f.a.kobe.config.contants.SystemContanst;
 
-public class UserSessionInterceptor implements HandlerInterceptor {
-	
-	private final static String ATTR_NAME = "userDetail";
-	
+public class UserSessionInterceptor implements HandlerInterceptor {	
 	
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
 		response.setCharacterEncoding("utf-8");;
 		response.setContentType("application/json; charset=utf-8");
-		if(request.getSession().getAttribute(ATTR_NAME) == null ) {
+		if(request.getSession().getAttribute(SystemContanst.USER_AGENT) == null ) {
 			response.setStatus(HttpStatus.SC_NON_AUTHORITATIVE_INFORMATION);
 			PrintWriter writer = response.getWriter();
 			JSONObject json = new JSONObject();
-			json.put("desc", "can not find userDetail from session");
+			json.put("desc", "Non Authoritative Information");
 			writer.write(json.toJSONString());
 			return false;
 		}
