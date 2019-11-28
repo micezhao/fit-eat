b937ead678ed773b2b97b3376a1578ce94090d62
micezhao <micezhao@zhaochendeMacbook-pro.local>
2019-11-28 15:26:41 +0800

    Update UserSessionInterceptor.java

--- fit&eat/kobe/src/main/java/com/f/a/kobe/config/webconfig/interceptor/UserSessionInterceptor.java
+++ fit&eat/kobe/src/main/java/com/f/a/kobe/config/webconfig/interceptor/UserSessionInterceptor.java
@@ -24,7 +24,7 @@ public class UserSessionInterceptor implements HandlerInterceptor {
 			json.put("desc", "Non Authoritative Information");
 			writer.write(json.toJSONString());
 			return false;
-		}
+		}        
 		return true;
 	}
 }
