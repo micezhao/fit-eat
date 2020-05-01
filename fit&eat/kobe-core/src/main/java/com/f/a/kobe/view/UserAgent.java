937c73491a964b0b73ee714c9346b4f94d3c1f9e
funkyyao <xinyuemoli@foxmail.com>
2020-05-01 12:40:32 +0800

    提交

--- fit&eat/kobe-core/src/main/java/com/f/a/kobe/view/UserAgent.java
+++ fit&eat/kobe-core/src/main/java/com/f/a/kobe/view/UserAgent.java
@@ -3,40 +3,82 @@ package com.f.a.kobe.view;
 import lombok.Data;
 import lombok.ToString;
 
+import java.io.Serializable;
+
 @Data
 @ToString
-public class UserAgent {
-	
-		// 用户编号
-	  	private Long customerId;
-	  	// 真是姓名
-	    private String realname;
-	    // 生日
-	    private String birthday;
-	    // 年龄
-	    private Integer age;
-	    // 昵称
-	    private String nickname;
-	    // 头像地址
-	    private String headimg;
-	    //当前积分
-	    private Integer sorce;
-	    // 联系方式
-	    private String mobile;
-	    // 微信id
-	    private String wxOpenid;
-	    // 阿里id
-	    private String aliOpenid;   
-	    // 登陆用户名
-	    private String username;
-	    // 登陆密码
-	    private String password;
-	    // 邮箱信息
-	    private String email;
-	    // 登陆渠道
-	    private String loginType;
-	    // 默认收货地址
-	    private String defaultAddrId;
-	    
-	
+public class UserAgent implements Serializable {
+
+	private String loginType;
+
+	/**
+	 * 用户账号
+	 */
+	private String userAccount;
+
+	/**
+	 * 关联手机号
+	 */
+	private String mobile;
+
+	/**
+	 * 微信凭证
+	 */
+	private String wxopenid;
+
+	/**
+	 * 支付宝凭证
+	 */
+	private String aliopenid;
+
+	/**
+	 * 用户类型
+	 */
+	private String userType;
+
+	/**
+	 * 登录密码
+	 */
+	private String loginPwd;
+
+	/**
+	 * 真实姓名
+	 */
+	private String realname;
+
+	/**
+	 * 出生日期
+	 */
+	private String birthday;
+
+	/**
+	 * 年龄
+	 */
+	private Integer age;
+
+	/**
+	 * 性别
+	 */
+	private String gender;
+
+	/**
+	 * 用户昵称
+	 */
+	private String nickname;
+
+	/**
+	 * 用户头像
+	 */
+	private String headimgUrl;
+
+	/**
+	 * 当前积分
+	 */
+	private Integer score;
+
+	/**
+	 * 用户手机号
+	 */
+	private String updateMobile;
+
 }
