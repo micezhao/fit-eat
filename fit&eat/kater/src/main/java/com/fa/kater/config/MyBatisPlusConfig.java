b4cbf0957fefce3780c6e513e97230335c18931c
micezhao <zc_wuhan@163.com>
2020-05-03 10:24:18 +0800

    mp 升级

--- fit&eat/kater/src/main/java/com/fa/kater/config/MyBatisPlusConfig.java
+++ fit&eat/kater/src/main/java/com/fa/kater/config/MyBatisPlusConfig.java
@@ -1,8 +1,34 @@
 package com.fa.kater.config;
 
+import com.baomidou.mybatisplus.core.injector.AbstractMethod;
+import com.baomidou.mybatisplus.core.injector.AbstractSqlInjector;
 import com.baomidou.mybatisplus.core.injector.ISqlInjector;
+import com.baomidou.mybatisplus.core.injector.methods.Delete;
+import com.baomidou.mybatisplus.core.injector.methods.DeleteBatchByIds;
+import com.baomidou.mybatisplus.core.injector.methods.DeleteById;
+import com.baomidou.mybatisplus.core.injector.methods.DeleteByMap;
+import com.baomidou.mybatisplus.core.injector.methods.Insert;
+import com.baomidou.mybatisplus.core.injector.methods.SelectBatchByIds;
+import com.baomidou.mybatisplus.core.injector.methods.SelectById;
+import com.baomidou.mybatisplus.core.injector.methods.SelectByMap;
+import com.baomidou.mybatisplus.core.injector.methods.SelectCount;
+import com.baomidou.mybatisplus.core.injector.methods.SelectList;
+import com.baomidou.mybatisplus.core.injector.methods.SelectMaps;
+import com.baomidou.mybatisplus.core.injector.methods.SelectMapsPage;
+import com.baomidou.mybatisplus.core.injector.methods.SelectObjs;
+import com.baomidou.mybatisplus.core.injector.methods.SelectOne;
+import com.baomidou.mybatisplus.core.injector.methods.SelectPage;
+import com.baomidou.mybatisplus.core.injector.methods.Update;
+import com.baomidou.mybatisplus.core.injector.methods.UpdateById;
 import com.baomidou.mybatisplus.extension.injector.LogicSqlInjector;
+import com.baomidou.mybatisplus.extension.injector.methods.LogicDeleteByIdWithFill;
 import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+
+import static java.util.stream.Collectors.toList;
+
+import java.util.List;
+import java.util.stream.Stream;
+
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,7 +47,31 @@ public class MyBatisPlusConfig {
 
     @Bean
     public ISqlInjector sqlInjector() {
-        return new LogicSqlInjector();
-    }
+    	return new AbstractSqlInjector() {
 
+			@Override
+			public List<AbstractMethod> getMethodList(Class<?> mapperClass) {
+				  return Stream.of(
+				            new Insert(),
+				            new Delete(),
+				            new DeleteByMap(),
+				            new DeleteById(),
+				            new DeleteBatchByIds(),
+				            new Update(),
+				            new UpdateById(),
+				            new SelectById(),
+				            new SelectBatchByIds(),
+				            new SelectByMap(),
+				            new SelectOne(),
+				            new SelectCount(),
+				            new SelectMaps(),
+				            new SelectMapsPage(),
+				            new SelectObjs(),
+				            new SelectList(),
+				            new SelectPage(),
+				            new LogicDeleteByIdWithFill()
+				        ).collect(toList());
+				    };
+        };
+    }
 }
