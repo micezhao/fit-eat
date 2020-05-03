701d5580e26ac975359fefd6973e51fff95cbb12
funkyyao <xinyuemoli@foxmail.com>
2020-05-03 19:39:32 +0800

    提交

---
+++ fit&eat/kater/src/main/java/com/fa/kater/config/UserAgentResolver.java
@@ -0,0 +1,33 @@
+package com.fa.kater.config;
+
+import com.alibaba.fastjson.JSONObject;
+import com.f.a.kobe.contants.Contants;
+import com.f.a.kobe.view.UserAgent;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+@Component
+public class UserAgentResolver implements HandlerMethodArgumentResolver {
+	
+	@Override
+	public boolean supportsParameter(MethodParameter parameter) {
+		if (parameter.getParameterType().isAssignableFrom(UserAgent.class)) {
+            return true;
+        }
+        return false;
+	}
+
+	@Override
+	public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
+			NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+		 Object obj= webRequest.getAttribute(Contants.USER_AGENT, RequestAttributes.SCOPE_SESSION);
+		 UserAgent agent =  JSONObject.parseObject( JSONObject.toJSONString(obj), UserAgent.class);
+		return agent;
+	}
+
+}
